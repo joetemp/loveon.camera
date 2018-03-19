14e26d78827da4a34c9413c8052deba1a44107be
Joe <joe.k.templeton@gmail.com>
2018-03-19 15:58:06 -0600

    Got rid of the vars in nav.js as they weren't needed.

--- js/nav.js
+++ js/nav.js
@@ -3,10 +3,10 @@
 		//
 
 		// init controller
-		var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter"}});
+		new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter"}});
 
 		// build scenes
-		var scene = new ScrollMagic.Scene({triggerElement: "#edit"})
+		new ScrollMagic.Scene({triggerElement: "#edit"})
 						.setClassToggle("#test", "hide")
 						.addIndicators({name: "Add .hide class"})
 						.addTo(controller);
