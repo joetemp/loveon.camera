68dcc9fa09d0577b7f2e59694acd4bc26a046889
Joe <joe.k.templeton@gmail.com>
2018-03-19 16:34:05 -0600

    Nav is now working perfectly. I just need to follow the pattern I did for the first two sections.

--- js/nav.js
+++ js/nav.js
@@ -3,15 +3,19 @@
 		//
 
 		// init controller
-		new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter"}});
 
 		// build scenes
-		new ScrollMagic.Scene({triggerElement: "#edit"})
+		new ScrollMagic.Scene({triggerElement: "#edit", triggerHook: "onEnter"})
 						.setClassToggle("#test", "hide")
 						.addIndicators({name: "Add .hide class"})
 						.addTo(controller);
 
-		new ScrollMagic.Scene({triggerElement: "#edit"})
+		new ScrollMagic.Scene({triggerElement: "#resolution", triggerHook: "onLeave", duration: "100%"})
 						.setClassToggle("#waldo", "active")
+						.addIndicators()
+						.addTo(controller);
+
+		new ScrollMagic.Scene({triggerElement: "#edit", triggerHook: "onLeave", duration: "100%"})
+						.setClassToggle("#fuck", "active")
 						.addIndicators()
 						.addTo(controller);
