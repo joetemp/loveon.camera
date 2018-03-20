88bb1f28e9f3d175d3bbcd491f748a7d21bece4d
Joe <joe.k.templeton@gmail.com>
2018-03-20 12:15:27 -0600

    Changed .active class system to trigger on center. The mechanics are now much better.

--- js/nav.js
+++ js/nav.js
@@ -10,27 +10,27 @@
 						.addIndicators({name: "Add .hide class"})
 						.addTo(controller);
 
-		new ScrollMagic.Scene({triggerElement: "#resolution", triggerHook: "onLeave", duration: "100%"})
+		new ScrollMagic.Scene({triggerElement: "#resolution", triggerHook: "onCenter", duration: "100%"})
 						.setClassToggle("#nav_4k", "active")
 						.addIndicators()
 						.addTo(controller);
 
-		new ScrollMagic.Scene({triggerElement: "#edit", triggerHook: "onLeave", duration: "100%"})
+		new ScrollMagic.Scene({triggerElement: "#edit", triggerHook: "onCenter", duration: "100%"})
 						.setClassToggle("#nav_edit", "active")
 						.addIndicators()
 						.addTo(controller);
 
-		new ScrollMagic.Scene({triggerElement: "#stills", triggerHook: "onLeave", duration: "100%"})
+		new ScrollMagic.Scene({triggerElement: "#stills", triggerHook: "onCenter", duration: "100%"})
 						.setClassToggle("#nav_stills", "active")
 						.addIndicators()
 						.addTo(controller);
 
-		new ScrollMagic.Scene({triggerElement: "#webpage", triggerHook: "onLeave", duration: "100%"})
+		new ScrollMagic.Scene({triggerElement: "#webpage", triggerHook: "onCenter", duration: "100%"})
 						.setClassToggle("#nav_webpage", "active")
 						.addIndicators()
 						.addTo(controller);
 
-		new ScrollMagic.Scene({triggerElement: "#book", triggerHook: "onLeave", duration: "100%"})
+		new ScrollMagic.Scene({triggerElement: "#book", triggerHook: "onCenter", duration: "100%"})
 						.setClassToggle("#nav_book", "active")
 						.addIndicators()
 						.addTo(controller);
