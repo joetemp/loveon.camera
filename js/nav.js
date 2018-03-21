5b796cc1eae457aa97717e30ef754a6a5ece090c
Joe <joe.k.templeton@gmail.com>
2018-03-21 14:58:23 -0600

    Fixed the first trigger indentation.

--- js/nav.js
+++ js/nav.js
@@ -21,7 +21,7 @@
 		// This assigns the .black_non class to all the nav buttons when the nav turns black.
 		new ScrollMagic.Scene({triggerElement: "#resolution", triggerHook: "onLeave"})
 						.setClassToggle(".ghost", "black_non")
-						.addIndicators({name: "black_non", indent: 300, colorStart: "#FF6B6B", colorTrigger: "rgba(255, 107, 107, 0.5)"})
+						.addIndicators({name: "black_non", colorStart: "#FF6B6B", colorTrigger: "rgba(255, 107, 107, 0.5)"})
 						.offset(-100)
 						.addTo(controller);
 
@@ -76,7 +76,7 @@
 		// This changes the .active class on the 4K nav button from white to black. 
 		new ScrollMagic.Scene({triggerElement: "#resolution", triggerHook: "onLeave", duration: "50%"})
 						.setClassToggle("#nav_4k", "black_active")
-						.addIndicators({name: "Dip Shitz", colorStart: "#FF6B6B"})
+						.addIndicators({name: "Dip Shitz", indent: 300, colorStart: "#FF6B6B"})
 						.offset(-100)
 						.addTo(controller);
 
