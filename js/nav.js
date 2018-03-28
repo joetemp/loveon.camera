ec58c48b16b3000f08e880ca8b7e63df2e2d35a9
Joe <joe.k.templeton@gmail.com>
2018-03-28 15:30:27 -0600

    Made a bunch of triggers, start and end markers gray. Also changed the duration of white_non and white_active in nav.js

--- js/nav.js
+++ js/nav.js
@@ -5,37 +5,37 @@
 		//
 
 		// This adds the .white_active class to the '4K' nav button. 
-		new ScrollMagic.Scene({triggerElement: "#resolution", triggerHook: "onCenter", duration: "100%"})
+		new ScrollMagic.Scene({triggerElement: "#resolution", triggerHook: "onCenter", duration: "50%"})
 						.setClassToggle("#nav_4k", "white_active")
-						.addIndicators({name: "white_active", colorTrigger: "green"})
+						.addIndicators({name: "white_active", colorStart: "rgba(102, 102, 102, 0.8)", colorTrigger: "rgba(102, 102, 102, 0.8)"})
 						.offset(-50)
 						.addTo(controller);
 
 		// This assigns the .white_non class to all the nav buttons when the 4K button becomes active (still white)
-		new ScrollMagic.Scene({triggerElement: "#resolution", triggerHook: "onCenter", duration: "100%"})
+		new ScrollMagic.Scene({triggerElement: "#resolution", triggerHook: "onCenter", duration: "50%"})
 						.setClassToggle(".ghost", "white_non")
-						.addIndicators({name: "white_non", indent: 115})
+						.addIndicators({name: "white_non", colorStart: "rgba(102, 102, 102, 0.8)", indent: 115})
 						.offset(-50)
 						.addTo(controller);
 
 		// This assigns the .black_non class to all the nav buttons when the nav turns black.
 		new ScrollMagic.Scene({triggerElement: "#resolution", triggerHook: "onLeave"})
 						.setClassToggle(".ghost", "black_non")
-						.addIndicators({name: "black_non", colorStart: "#FF6B6B", colorTrigger: "rgba(102, 102, 102, 0.8)"})
+						.addIndicators({name: "black_non", colorStart: "rgba(102, 102, 102, 0.8)", colorTrigger: "rgba(102, 102, 102, 0.8)"})
 						.offset(-100)
 						.addTo(controller);
 
 		// This makes the header bar hide 
 		new ScrollMagic.Scene({triggerElement: "#resolution", triggerHook: "onLeave"})
 						.setClassToggle("#test", "hide")
-						.addIndicators({name: "hide", indent: 420, colorStart: "#FF6B6B"})
+						.addIndicators({name: "hide", indent: 420, colorStart: "rgba(102, 102, 102, 0.8)"})
 						.offset(-100)
 						.addTo(controller);
 
 		// This sets the .black_active class according to what section is currently on the screen.
 		new ScrollMagic.Scene({triggerElement: "#edit", triggerHook: "onCenter", duration: 100})
 						.setClassToggle("#nav_4k", "gap")
-						.addIndicators({name: "4k Gap", indent: 60})
+						.addIndicators({name: "4k Gap", indent: 60, colorStart: "rgba(102, 102, 102, 0.8)"})
 						.offset(-100)
 						.addTo(controller);
 
@@ -62,20 +62,20 @@
 		// This changes the font color on the nav
 		new ScrollMagic.Scene({triggerElement: "#resolution", triggerHook: "onLeave"})
 						.setClassToggle("a", "black")
-						.addIndicators({name: "text black", indent: 206, colorStart: "#FF6B6B"})
+						.addIndicators({name: "text black", indent: 206, colorStart: "rgba(102, 102, 102, 0.8)"})
 						.offset(-100)
 						.addTo(controller);
 
 		// This changes the logo color 
 		new ScrollMagic.Scene({triggerElement: "#resolution", triggerHook: "onLeave"})
 						.setClassToggle(".heart", "murder")
-						.addIndicators({name: "black logo", indent: 102, colorStart: "#FF6B6B"})
+						.addIndicators({name: "black logo", indent: 102, colorStart: "rgba(102, 102, 102, 0.8)"})
 						.offset(-100)
 						.addTo(controller);
 
 		// This changes the .active class on the 4K nav button from white to black. 
 		new ScrollMagic.Scene({triggerElement: "#resolution", triggerHook: "onLeave", duration: "50%"})
 						.setClassToggle("#nav_4k", "black_active")
-						.addIndicators({name: "black_active", indent: 305, colorStart: "#FF6B6B"})
+						.addIndicators({name: "black_active", indent: 305, colorStart: "rgba(102, 102, 102, 0.8)", colorEnd: "rgba(102, 102, 102, 0.8)"})
 						.offset(-100)
 						.addTo(controller);
