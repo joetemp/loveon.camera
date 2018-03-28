30a985da04fba41187304060e02c85cc83cfdd5d
Joe <joe.k.templeton@gmail.com>
2018-03-28 15:31:17 -0600

    Changed trigger color in bg.js

--- js/bg.js
+++ js/bg.js
@@ -4,5 +4,5 @@ var controller = new ScrollMagic.Controller({});
 // build scenes
 new ScrollMagic.Scene({triggerElement: "#video", triggerHook: "onEnter", duration: "200%"})
 				.setTween("#video > video", {y: "80%", ease: Linear.easeNone})
-				.addIndicators({name: "bg"})
+				.addIndicators({name: "bg", colorTrigger: "rgba(102, 102, 102, 0.8)"})
 				.addTo(controller);
