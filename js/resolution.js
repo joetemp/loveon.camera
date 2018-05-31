ac36ffce90e56617e60dd1b9d73dd1602633147d
Joe <joe.k.templeton@gmail.com>
2018-05-31 11:54:50 -0600

    Deleted the elements I was trying to manipulate in the resolution section. Something REALLY funky is going on with it. Just going to do static images for now.

--- js/resolution.js
+++ js/resolution.js
@@ -3,6 +3,6 @@ var controller = new ScrollMagic.Controller({});
 
 // build scenes
 new ScrollMagic.Scene({triggerElement: "#resolution"})
-				.setClassToggle("#altar", "appear")
+				.setClassToggle("#blue", "appear")
 				.offset(-50)
 				.addTo(controller);
