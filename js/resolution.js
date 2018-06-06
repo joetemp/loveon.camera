a9b58d0390bba49aae63de187aed2c5dd5734d89
Joe <joe.k.templeton@gmail.com>
2018-06-06 09:13:44 -0600

    Change the names of #altar and .appear to #res-id and .res-class. Now going to try to make this work better.

--- js/resolution.js
+++ js/resolution.js
@@ -3,6 +3,6 @@ var controller = new ScrollMagic.Controller({});
 
 // build scenes
 new ScrollMagic.Scene({triggerElement: "#resolution"})
-				.setClassToggle("#altar", "appear")
+				.setClassToggle("#res-id", "res-class")
 				.offset(-50)
 				.addTo(controller);
