f17cf664b2cb651c94ef07cf535cab34383af6d7
Joe <joe.k.templeton@gmail.com>
2018-06-06 09:30:02 -0600

    Gonna ditch the scroll effects on the section images. Cannot get the class to toggle properly.

--- js/resolution.js
+++
@@ -1,8 +0,0 @@
-// init controller
-var controller = new ScrollMagic.Controller({});
-
-// build scenes
-new ScrollMagic.Scene({triggerElement: "#resolution"})
-				.setClassToggle("#res-id", "res-class")
-				.offset(-50)
-				.addTo(controller);
